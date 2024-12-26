782c68d400cd38dc60319f53ecb6248d2784161b
duonghieu0712z <duonghieu0712z@gmail.com>
2024-12-26 16:36:17 +0700

    feat: add build asm.js

--- make-debug.sh
+++ make-debug.sh
@@ -5,8 +5,12 @@ rm sdk_source_bin/physx-js-webidl.*
 make -j8
 
 mkdir -p ../../../../dist-debug/
+
 cp sdk_source_bin/physx-js-webidl.js ../../../../dist-debug/
 cp sdk_source_bin/physx-js-webidl.wasm ../../../../dist-debug/
+
+cp sdk_source_bin/physx-js-webidl.asm.js ../../../../dist/
+cp sdk_source_bin/physx-js-webidl.asm.js.mem ../../../../dist/
 
 # if [ -d "/src/types" ]; then
 #     cp /src/types/physx-js-webidl.d.ts /src/dist-debug/
--- make-profile.sh
+++ make-profile.sh
@@ -5,8 +5,12 @@ rm sdk_source_bin/physx-js-webidl.*
 make -j8
 
 mkdir -p ../../../../dist-profile/
+
 cp sdk_source_bin/physx-js-webidl.js ../../../../dist-profile/
 cp sdk_source_bin/physx-js-webidl.wasm ../../../../dist-profile/
+
+cp sdk_source_bin/physx-js-webidl.asm.js ../../../../dist/
+cp sdk_source_bin/physx-js-webidl.asm.js.mem ../../../../dist/
 
 # if [ -d "/src/types" ]; then
 #     cp /src/types/physx-js-webidl.d.ts /src/dist-profile/
